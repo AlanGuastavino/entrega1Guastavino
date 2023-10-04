31234af82ec852d14470afbcf7cab23c955c4dd6
AlanGuastavino <127367062+AlanGuastavino@users.noreply.github.com>
2023-10-04 19:34:50 -0300

    Delete src directory

--- src/components/ItemListContainer.js
+++
@@ -1,9 +0,0 @@
-const ItemListContainer = ({ greeting }) => {
-  return (
-    <div className="item-list-container">
-      <p>{greeting}</p>
-    </div>
-  );
-};
-
-export default ItemListContainer;
