4e6edd7342da03def6f1cb9d4d4c82538c1fef12
AlanGuastavino <127367062+AlanGuastavino@users.noreply.github.com>
2023-10-04 19:36:29 -0300

    Add files via upload
    subiendo archivos nuevos

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,10 @@
+import cartw from "./assets/cartw.svg";
+const CartWidget = () => {
+  return (
+    <div>
+      <img src={cartw} alt="cart-widget" />5
+    </div>
+  );
+};
+
+export default CartWidget;
